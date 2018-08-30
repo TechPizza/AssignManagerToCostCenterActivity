746404f3404dde64cd4eb0dc1f0d22be09cc5b95
Pizzayolo <dvizzari92@gmail.com>
2018-08-30 07:45:03 +0000

    Init

---
+++ AssignManagerToCostCenterActivity/AssignManagerToCostCenterActivity.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Reflection;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace FIM.AssignManagerToCostCenterActivity
+{
+    public partial class AssignManagerToCostCenterActivity
+    {
+        #region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("", "")]
+        private void InitializeComponent()
+        {
+            this.CanModifyActivities = true;
+            System.Workflow.ComponentModel.ActivityBind activitybind1 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind2 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind3 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind4 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind5 = new System.Workflow.ComponentModel.ActivityBind();
+            this.InitializeEnumUser = new System.Workflow.Activities.CodeActivity();
+            this.readUser = new Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity();
+            this.initializeReadCostCenter = new System.Workflow.Activities.CodeActivity();
+            this.ReadCurrentRequest = new Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity();
+            // 
+            // InitializeEnumUser
+            // 
+            this.InitializeEnumUser.Name = "InitializeEnumUser";
+            this.InitializeEnumUser.ExecuteCode += new System.EventHandler(this.InitializeEnumUser_ExecuteCode);
+            // 
+            // readUser
+            // 
+            activitybind1.Name = "AssignManagerToCostCenterActivity";
+            activitybind1.Path = "readUser_ActorId1";
+            this.readUser.Name = "readUser";
+            activitybind2.Name = "AssignManagerToCostCenterActivity";
+            activitybind2.Path = "readUser_Resource1";
+            activitybind3.Name = "AssignManagerToCostCenterActivity";
+            activitybind3.Path = "readUser_ResourceId1";
+            activitybind4.Name = "AssignManagerToCostCenterActivity";
+            activitybind4.Path = "readUser_SelectionAttributes1";
+            this.readUser.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ActorIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind1)));
+            this.readUser.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ResourceProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind2)));
+            this.readUser.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ResourceIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind3)));
+            this.readUser.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.SelectionAttributesProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind4)));
+            // 
+            // initializeReadCostCenter
+            // 
+            this.initializeReadCostCenter.Name = "initializeReadCostCenter";
+            this.initializeReadCostCenter.ExecuteCode += new System.EventHandler(this.initializeReadCostCenter_ExecuteCode);
+            // 
+            // ReadCurrentRequest
+            // 
+            activitybind5.Name = "AssignManagerToCostCenterActivity";
+            activitybind5.Path = "ReadCurrentRequest_CurrentRequest1";
+            this.ReadCurrentRequest.Name = "ReadCurrentRequest";
+            this.ReadCurrentRequest.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity.CurrentRequestProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind5)));
+            // 
+            // AssignManagerToCostCenterActivity
+            // 
+            this.Activities.Add(this.ReadCurrentRequest);
+            this.Activities.Add(this.initializeReadCostCenter);
+            this.Activities.Add(this.readUser);
+            this.Activities.Add(this.InitializeEnumUser);
+            this.Name = "AssignManagerToCostCenterActivity";
+            this.CanModifyActivities = false;
+
+        }
+
+        #endregion
+
+        private CodeActivity initializeReadCostCenter;
+
+        private Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity readUser;
+
+        private CodeActivity InitializeEnumUser;
+
+        private Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity ReadCurrentRequest;
+
+
+
+
+
+    }
+}
---
+++ AssignManagerToCostCenterActivity/AssignManagerToCostCenterActivity.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Linq;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+using Microsoft.ResourceManagement.WebServices.WSResourceManagement;
+using System.Collections.ObjectModel;
+using Microsoft.ResourceManagement.Workflow.Activities;
+
+namespace FIM.AssignManagerToCostCenterActivity
+{
+    public partial class AssignManagerToCostCenterActivity : SequenceActivity
+    {
+        private static readonly Guid FIMADMGUID = new Guid("4074f258-a177-42df-b8ca-cba1a0cefb25");
+
+        private Guid _requestor;
+        private Guid _target;
+
+        #region ReadCurrentRequest
+        public static DependencyProperty ReadCurrentRequest_CurrentRequest1Property = DependencyProperty.Register("ReadCurrentRequest_CurrentRequest1", typeof(Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType), typeof(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Misc")]
+        public Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType ReadCurrentRequest_CurrentRequest1
+        {
+            get
+            {
+                return ((Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType)(base.GetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.ReadCurrentRequest_CurrentRequest1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.ReadCurrentRequest_CurrentRequest1Property, value);
+            }
+        }
+        #endregion
+        #region ReadUser
+        public static DependencyProperty readUser_ActorId1Property = DependencyProperty.Register("readUser_ActorId1", typeof(System.Guid), typeof(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid readUser_ActorId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_ActorId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_ActorId1Property, value);
+            }
+        }
+
+        public static DependencyProperty readUser_Resource1Property = DependencyProperty.Register("readUser_Resource1", typeof(Microsoft.ResourceManagement.WebServices.WSResourceManagement.ResourceType), typeof(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Microsoft.ResourceManagement.WebServices.WSResourceManagement.ResourceType readUser_Resource1
+        {
+            get
+            {
+                return ((Microsoft.ResourceManagement.WebServices.WSResourceManagement.ResourceType)(base.GetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_Resource1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_Resource1Property, value);
+            }
+        }
+
+        public static DependencyProperty readUser_ResourceId1Property = DependencyProperty.Register("readUser_ResourceId1", typeof(System.Guid), typeof(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid readUser_ResourceId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_ResourceId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_ResourceId1Property, value);
+            }
+        }
+
+        public static DependencyProperty readUser_SelectionAttributes1Property = DependencyProperty.Register("readUser_SelectionAttributes1", typeof(System.String[]), typeof(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public String[] readUser_SelectionAttributes1
+        {
+            get
+            {
+                return ((string[])(base.GetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_SelectionAttributes1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.AssignManagerToCostCenterActivity.AssignManagerToCostCenterActivity.readUser_SelectionAttributes1Property, value);
+            }
+        }
+        #endregion
+
+        public AssignManagerToCostCenterActivity()
+        {
+            InitializeComponent();
+        }
+
+        private void initializeReadCostCenter_ExecuteCode(object sender, EventArgs e)
+        {
+            RequestType currentRequest = this.ReadCurrentRequest.CurrentRequest;
+            ReadOnlyCollection<CreateRequestParameter> requestParameters = currentRequest.ParseParameters<CreateRequestParameter>();
+
+            // Retrieve current workflow
+            SequentialWorkflow workflow = null;
+            if (!SequentialWorkflow.TryGetContainingWorkflow(this, out workflow))
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Save information
+            _requestor = workflow.ActorId;
+            _target = workflow.TargetId;
+
+            // Init ReadUser activity
+            this.readUser_ActorId1 = FIMADMGUID;
+            this.readUser_ResourceId1 = _target;
+        }
+
+        private void InitializeEnumUser_ExecuteCode(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
